84defec6efb4a3365d3d0aa510e918798dc8a59a
Raphael Busquet <raphaelbusquet@outlook.com>
2023-05-20 23:40:44 +0100

    innitial commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const Welcome = () => {
+  const router = useRouter()
+
+  return (
+    <View>
+      <View
+        style={styles.container}
+      >
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
